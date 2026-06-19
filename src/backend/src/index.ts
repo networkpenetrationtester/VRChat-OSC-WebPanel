412d1f448d7cbecb3183aff46c3976c2380611a5
Neo <77405346+networkpenetrationtester@users.noreply.github.com>
2026-06-18 19:26:59 -0500

    feat: demo

--- src/backend/src/index.ts
+++ src/backend/src/index.ts
@@ -1,72 +1,54 @@
-// import { VRC_ADDRESS, VRC_RX_PORT, VRC_TX_PORT, INTERFACE_ADDRESS } from './constants.ts';
-// import { STDIO } from './modules.ts';
-// import { VRChatOSCRouter } from './osc_router.ts';
-// import type { $VRChatOSCRouterExternalApplication } from './types.ts';
-
-// const app: $VRChatOSCRouterExternalApplication = {
-// 	address: '192.168.1.147',
-// 	port: 9001,
-// 	name: 'test'
-// };
-
-// const Router = new VRChatOSCRouter();
-
-// Router.Route('/*_', app);
-
-// Router.Create({
-// 	VRC_ADDRESS,
-// 	VRC_RX_PORT,
-// 	VRC_TX_PORT,
-// 	INTERFACE_ADDRESS
-// });
-
-// const stdio = STDIO();
-
-// stdio.on('line', async input => {
-// 	const [address, ...values] = input.split(' ');
-// 	await Router.SendValueAcknowledged(address, ...values);
-// });
-
-// export { Router };
-
-// const fileWatcher = fs.watchFile('test.txt', (curr, prev) => { // it's kinda slow
-// 	console.log(curr);
-// 	fs.existsSync('test.txt') && console.log(fs.readFileSync('test.txt', 'utf8'));
-// });
-
 import fs from 'node:fs';
 import path from 'node:path';
-import { VRC_TEMP_DIR } from './constants.ts';
+import { INTERFACE_ADDRESS, VRC_ADDRESS, VRC_RX_PORT, VRC_TEMP_DIR, VRC_TX_PORT } from './constants.ts';
 import type { $AmplitudeCache, $AmplitudeSettings } from './types_amplitude.ts';
+import { VRChatOSCRouter } from './osc_router.ts';
+import { STDIO } from './modules.ts';
+import type { $MessageListenerCallback } from './types.ts';
+import { VRChatOSCInterface } from './osc_interface.ts';
 
-export const AmplitudeCache: $AmplitudeCache = [];
+// export const AmplitudeCache: $AmplitudeCache = [];
 
-const AmplitudeWatcher = fs.watch(VRC_TEMP_DIR, (event, filename) => {
-	console.log(event, filename);
-	if (event === 'change') {
-		if (filename) {
-			const filepath = path.join(VRC_TEMP_DIR, filename);
-			const data = fs.readFileSync(filepath, 'utf-8');
-			const stat = fs.statSync(filepath);
-			try {
-				switch (filename) {
-					case 'amplitude.cache': {
-						if (stat.size <= 2) return;
-						const amplitude_cache: $AmplitudeCache = JSON.parse(data);
-						console.log('CAHE:', amplitude_cache);
-						console.log(amplitude_cache.pop()?.user_properties.currentAvatarId);
-						return;
-					}
-					case 'com_settings.amplitude': {
-						// if (stat.size <= 2) return;
-						const amplitude_settings: $AmplitudeSettings = JSON.parse(data);
-						console.log('SETTINGS:', amplitude_settings);
-						return;
-					}
-					default: {
-					}
-				}
-			} catch {}
-		}
-	}
+// const AmplitudeWatcher = fs.watch(VRC_TEMP_DIR, (event, filename) => {
+// 	console.log(event, filename);
+// 	if (event === 'change') {
+// 		if (filename) {
+// 			const filepath = path.join(VRC_TEMP_DIR, filename);
+// 			const data = fs.readFileSync(filepath, 'utf-8');
+// 			const stat = fs.statSync(filepath);
+// 			try {
+// 				switch (filename) {
+// 					case 'amplitude.cache': {
+// 						if (stat.size <= 2) return;
+// 						const amplitude_cache: $AmplitudeCache = JSON.parse(data);
+// 						// console.log('CAHE:', amplitude_cache);
+// 						console.log(amplitude_cache.pop()?.user_properties.currentAvatarId);
+// 						return;
+// 					}
+// 					case 'com_settings.amplitude': {
+// 						// if (stat.size <= 2) return;
+// 						const amplitude_settings: $AmplitudeSettings = JSON.parse(data);
+// 						console.log('SETTINGS:', amplitude_settings);
+// 						return;
+// 					}
+// 					default: {
+// 					}
+// 				}
+// 			} catch { }
+// 		}
+// 	}
+// });
+
+const int = new VRChatOSCInterface({
+	VRC_ADDRESS,
+	VRC_RX_PORT,
+	VRC_TX_PORT,
+	INTERFACE_ADDRESS
+});
+
+int.Create();
+
+STDIO().on('line', async line => {
+	const [address, ...values] = line.split(' ');
+	await int.SendValueAcknowledged(address, ...values);
 });
